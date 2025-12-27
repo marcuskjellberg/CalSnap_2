b3fe8d64d1e63a1b743960f0fc778e2189038d53
Marcus <marcus.p.kjellberg@gmail.com>
2025-12-27 20:13:32 +0100

    Initial Commit

---
+++ CalSnap/CalSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalSnapApp.swift
+//  CalSnap
+//
+//  Created by Marcus on 2025-12-27.
+//
+
+import SwiftUI
+
+@main
+struct CalSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
